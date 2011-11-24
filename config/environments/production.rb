01f61fbcbb95232abb5369b917fbd23c3c957f50
liwenjie <sc_aluo@163.com>
2011-11-24 22:51:26 +0800

    modi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,18 @@ Microforum::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  config.action_mailer.default_url_options={:host=>'localhost:3000'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+       :address => "smtp.163.com",
+       :port => 25,
+       :domain => "163.com",
+       :authentication => :login,
+       :user_name => "sc_aluo@163.com",
+       :password => "monkey_71"
+     }
+
 end
