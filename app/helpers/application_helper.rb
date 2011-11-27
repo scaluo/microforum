3e79093ed4a0ef3b3bf26e850f10f307b5a29a45
liwenjie <sc_aluo@163.com>
2011-11-27 16:55:32 +0800

    modi title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
-
+  def title (*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts<<"Micro Forum").join("-")  unless parts.empty?
+      end
+    end
+  end
 end
